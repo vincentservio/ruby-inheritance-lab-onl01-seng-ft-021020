ae8240ca9c92f9f0b1a44feb86a947a6a9805c3b
Vincent Servio <vincentservio@gmail.com>
2020-02-25 23:42:26 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,16 @@
-class Teacher
-
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-
+class Teacher < User
+attr_accessor :knowledge
+  KNOWLEDGE = ["a String is a type of data in Ruby",
+               "programming is hard, but it's worth it",
+               "javacsript async web request",
+               "Ruby method call definition",
+               "object oriented dog cat class instance",
+               "class method class variable instance method instance varaible",
+               "programming computers hacking learning terminal",
+               "bash Ruby rvm update certs"]
+  def teach 
+    KNOWLEDGE.sample
+  end
+  
   
 end
